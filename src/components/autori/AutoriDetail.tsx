a7140e552df1445212b1031c93a631ecaae54f16
Xuan Anh Duong <thanhniennghiem98@gmail.com>
2019-11-12 21:07:11 +0100

    zobrazovani detail autora z DB

--- src/components/autori/AutoriDetail.tsx
+++ src/components/autori/AutoriDetail.tsx
@@ -1,75 +1,134 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Spin } from 'antd';
+import axios from 'axios';
+import { connect } from 'react-redux';
+import { Reducer } from '../../utils/generalTypes';
 
 interface Props {
-
+    match: any
+    location: any
+    history: any
+    reducer?: Reducer
+    dispatch?: Function
 }
 
 interface State {
+    autor: any
+    loading: boolean
 }
 
 class AutoriDetail extends Component<Props, State>{
     constructor(props: Props) {
         super(props);
         this.state = {
+            autor: {},
+            loading: false
 
         }
     }
+
+    componentDidMount() {
+        this.getAutorDetail();
+    }
+
     render() {
         return (
             <React.Fragment>
-                <Row>
-                    <Col span={8}>
+                {this.state.loading ?
+                    <div
+                        style={{
+                            position: "fixed",
+                            zIndex: 1501,
+                            background: "#000000",
+                            height: "100vh",
+                            opacity: 0.5,
+                            width: "100%",
 
-                        <img
-                            style={{
-                                height: "auto",
-                                width: "100%",
-                                padding: "2em",
-                                borderRadius: "4em"
-                            }}
-                            alt="logo" src="https://www.kniznice.cz/media/k2/items/cache/461011bcaf5b5e723e897c33f049f169_XL.jpg" />
+                        }}>>
+                        <Spin tip="Načítání autora..." size="large" style={{
+                            fontSize: '1.5em', 
+                            position: 'absolute',
+                            left: '50%',
+                            top: '45%',
+                            "transform":"translate(-50%, -50%)"
+                        }}></Spin>
+                    </div>
+                    :
+                    <Row>
+                        <Col span={8}>
+                            <img
+                                style={{
+                                    height: "auto",
+                                    width: "100%",
+                                    padding: "2em",
+                                    borderRadius: "4em"
+                                }}
+                                alt="logo" src={this.state.autor.img} />
 
-                    </Col>
-                    <Col span={16}>
-                        <div style={{ padding: "2em" }}>
-                             <h1 style={{ fontSize: "2em" }}>Viktor Dyk <span>(1877 - 1931)</span></h1> 
-                        </div>
-                        <Row>
-                            <Col span={8} style={{ paddingRight:"1em" }}>
-                                <h3>Zařazení:</h3>
-                                <li>Český autor 1. poloviny 20. století.</li>
-                                <li>Před 1. světovou válkou patřil k anarchistickým buřičům.</li>
-                                <li>Básník, prozaik a dramatik.</li>
-                                <li>Věnoval se i publicistice a zapojoval se aktivně do politickýho a veřejného života.</li>
-                                <li>V anarchistickým obdobý psal hlavně poezie-básně vyjadřující jeho nesouhlas sestavem české společností (Satira a ironie).</li>
-                                <li>Později převažuje próza - častý motiv - vlastenectví a národní hrdost (Zdůrazňoval právo národa na samostatnost).</li>
-                               
-                            </Col>
-                            <Col span={8} style={{ paddingRight:"1em" }}>
-                                <h3>Život:</h3>
-                                <li>Narozen v roku 1877.</li>
-                                <li>Pocházel z venkova (vesnice Pšovka u Mělníka).</li>
-                                <li>Vystudoval gymnázium a právnickou fakultu v Praze.</li>
-                                <li>Jediný z anarchistických buřiců, který dokončil studia na universitě.</li>
-                                <li>Po ukončení studií - publicistická práce a politické aktivity.</li>
-                                <li>Za 1. světový války za politický nízory byl vězněn.</li>
-                                <li>Po válce pokračoval v literárni a publicistické činnosti i v politických aktivitách (Člen Narodné demokratické strany).</li>
-                                <li>Tragicky zahynul roku 1931 na dovolené v Jugoslávii (utopil se).</li>
-                            </Col>
-                            <Col span={8} style={{ paddingRight:"1em" }}>
-                                <h3>Díla:</h3>
-                                    I. Próza
-                                     <li></li>
-                                
-                            </Col>
-                        </Row>
-                    </Col>
-                </Row>
-
+                        </Col>
+                        <Col span={16}>
+                            <div style={{ padding: "2em" }}>
+                                <h1 style={{ fontSize: "2em" }}>{this.state.autor.name} <span>({this.state.autor.born} - {this.state.autor.rip})</span></h1>
+                            </div>
+                            <Row>
+                                <Col span={8} style={{ paddingRight: "1em" }}>
+                                    <h3>Zařazení:</h3>
+                                    {this.state.autor.zarazeni !== undefined ?
+                                        this.state.autor.zarazeni.map((value: string, key: number) => <li>{value}</li>)
+                                        :
+                                        <div></div>
+                                    }
+                                </Col>
+                                <Col span={8} style={{ paddingRight: "1em" }}>
+                                    <h3>Život:</h3>
+                                    {this.state.autor.zivot !== undefined ?
+                                        this.state.autor.zivot.map((value: string, key: number) => <li>{value}</li>)
+                                        :
+                                        <div></div>
+                                    }
+                                </Col>
+                                <Col span={8} style={{ paddingRight: "1em" }}>
+                                    <h3>Díla:</h3>
+                                    {this.state.autor.druhDila !== undefined ?
+                                        this.state.autor.druhDila.map(
+                                            (value: string, key: number) =>
+                                                <div>
+                                                    {value}
+                                                </div>
+                                        )
+                                        :
+                                        <div></div>
+                                    }
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                }
             </React.Fragment>
         );
     }
+
+    private getAutorDetail() {
+        this.setState({
+            loading: true
+        });
+        const id: number = parseInt(this.props.match.params.id, 10);;
+        axios({
+            method: 'get',
+            url: '/autor/' + id,
+            withCredentials: true,
+        })
+            .then(
+                res => {
+
+                    this.setState({
+                        autor: res.data[0],
+                        loading: false
+                    });
+
+                }
+            ).catch(err => err)
+    }
 }
 
-export default AutoriDetail;
+export default (connect(reducer => reducer)(AutoriDetail));
