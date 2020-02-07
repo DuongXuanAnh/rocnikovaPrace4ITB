81a6e7e3c356f9b54b57ee56db1f60f7c3af23f2
Xuan Anh Duong <thanhniennghiem98@gmail.com>
2020-02-07 21:30:16 +0100

    pridavani Kvizu, zakladni rozmisteni

--- src/components/kviz/Kviz.tsx
+++ src/components/kviz/Kviz.tsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { Row, Col, Button } from 'antd';
+import { withRouter } from 'react-router';
+import { connect } from 'react-redux';
+import axios from 'axios';
 
 interface Props {
 
@@ -12,17 +16,73 @@ class Kviz extends Component<Props, State> {
 
     constructor(props: Props) {
         super(props);
-        
+
+    }
+
+    componentDidMount() {
+        this.getQuestion();
     }
     
 
     render() {
         return (
             <React.Fragment>
-                Kviz
-            </React.Fragment>
+                <Row
+                    style={{
+                        background: "#FFFFFF",
+                        margin: "2em 2em 0 2em",
+                        minHeight: "7em",
+                        borderRadius: "1em",
+                        textAlign: "center"
+                    }}>
+                    <Col span={24} style={{ height: "100%", fontSize: "2em", lineHeight: "3em" }}>
+                        Kdy se narodil Karel Capek?
+                    </Col>
+                </Row>
+                <Row style={{ textAlign: "center", height: "5em", margin: "2em", borderRadius: "1em", lineHeight: "5em" }}>
+
+                    <Col span={12} style={{ height: "100%", fontSize: "2em", lineHeight: "3em", marginBottom: "5px" }}>
+                        <Button style={{
+                            height: "100%",
+                            width: "99%",
+                            fontSize: "1em"
+                        }}>1890</Button>
+                    </Col>
+                    <Col span={12} style={{ height: "100%", fontSize: "2em", lineHeight: "3em", marginBottom: "5px" }}>
+                        <Button style={{
+                            height: "100%",
+                            width: "99%",
+                            fontSize: "1em"
+                        }}>1890</Button>
+                    </Col>
+                    <Col span={12} style={{ height: "100%", fontSize: "2em", lineHeight: "3em" }}>
+                        <Button style={{
+                            height: "100%",
+                            width: "99%",
+                            fontSize: "1em"
+                        }}>1890</Button>
+                    </Col>
+                    <Col span={12} style={{ height: "100%", fontSize: "2em", lineHeight: "3em" }}>
+                        <Button style={{
+                            height: "100%",
+                            width: "99%",
+                            fontSize: "1em"
+                        }}>1890</Button>
+                    </Col>
+                </Row>
+            </React.Fragment >
         );
+    }
+
+    private getQuestion = () => {
+        axios({
+            method: 'get',
+            url: '/kviz',
+            withCredentials: true,
+        }).then((res) => {
+            console.log(res.data);
+        }).catch(err => err)
     }
 }
 
-export default Kviz;
+export default withRouter((connect(reducer => reducer)(Kviz)));
